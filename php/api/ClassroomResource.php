5f34b6772258849d47589d770d83582fd40f2d87
mikep8595 <mikep8595@gmail.com>
2017-02-28 15:30:00 -0500

    Uncommented classroom resource file

--- php/api/ClassroomResource.php
+++ php/api/ClassroomResource.php
@@ -48,10 +48,10 @@ switch($_SERVER['REQUEST_METHOD']){
    $data['capacity'] = $put['capacity'];
    $id = $put['id'];
    //echo "<script>console.log( 'Debug Objects: " . $data['building_id'] . '\t' . $data['room_type_id'] . '\t' . $data['class_number'] . '\t' . $data['capacity'] . '\t'. "' );</script>";
-   /*if(dataCheck($data)){
+   if(dataCheck($data)){
      $message = classroomResourceRun('PUT', $id, $data, $dbc);
    }
-   else{ $message = "Data not in correct format";}*/
+   else{ $message = "Data not in correct format";}
    $message = "Data not in correct format";
    break;
 
